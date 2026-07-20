116e9dd41ccec96f479ef2c0f5bb7252d3225c6f
Sumanth Kamath <145032252+S-Kamath-01@users.noreply.github.com>
2026-07-20 23:21:22 +0530

    Add: minimum-stack - submission-1

---
+++ Data Structures & Algorithms/minimum-stack/submission-1.cpp
@@ -0,0 +1,29 @@
+class MinStack {
+public:
+    //Two Stacks Approach
+    stack<int> stk;
+    stack<int> minStack;
+
+    MinStack() {}
+    
+    void push(int val) {
+        stk.push(val);
+        val = min(val,minStack.empty()?val:minStack.top());
+        minStack.push(val);
+    }
+    
+    void pop() {
+        stk.pop();
+        minStack.pop();
+    }
+    
+    int top() {
+        return stk.top();
+    }
+    
+    int getMin() {
+        return minStack.top();
+    }
+    //Time Complexity - O(1) for all operations
+    //Space Complexity - O(n)
+};
