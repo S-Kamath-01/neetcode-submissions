afab2466e7404a0ea3dcacc377dd248b55f0a031
Sumanth Kamath <145032252+S-Kamath-01@users.noreply.github.com>
2026-07-29 11:16:17 +0530

    Add: house-robber - submission-2

---
+++ Data Structures & Algorithms/house-robber/submission-2.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    // Tabulation
+    int dfs(vector<int>&nums, vector<int>&dp){
+        if(nums.empty()) return 0;
+        if(nums.size()==1) return nums[0];
+        dp[0] = nums[0];
+        dp[1] = max(nums[0],nums[1]);
+
+        for(int i = 2; i<nums.size(); i++){
+            dp[i] = max(dp[i-1], nums[i] + dp[i-2]);
+        }
+        return dp[nums.size()-1];
+    }
+
+    int rob(vector<int>& nums) {
+        vector<int> dp(nums.size(), -1);
+        return dfs(nums,dp);
+    }
+    // Time Complexity - O(N)
+    // Space Complexity - O(N) for dp array    
+};
