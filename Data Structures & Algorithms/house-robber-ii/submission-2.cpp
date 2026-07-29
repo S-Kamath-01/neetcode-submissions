a8d96646f49f9d2beb9e9aaa45d2b216e553986f
Sumanth Kamath <145032252+S-Kamath-01@users.noreply.github.com>
2026-07-29 15:20:58 +0530

    Add: house-robber-ii - submission-2

---
+++ Data Structures & Algorithms/house-robber-ii/submission-2.cpp
@@ -0,0 +1,26 @@
+class Solution {
+// Space Optimized 
+private:
+    int solve(vector<int>& nums, int start , int end){
+        int prev2 = 0;
+        int prev1 = 0;
+        for(int i = start;i<=end;i++){
+            int current = max(prev1, nums[i]+prev2);
+            prev2 = prev1;
+            prev1 = current;
+        }
+        return prev1;
+    }
+public:
+
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if(n==0) return 0;
+        if(n==1) return nums[0];
+
+        int plan1 = solve(nums,0,n-2);
+        int plan2 = solve(nums,1, n-1);
+
+        return max(plan1,plan2);
+    }
+};
